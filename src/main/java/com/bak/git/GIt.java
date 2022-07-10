06016c1b572d436dd6a6b1d95c0b4e051990b456
jlh <jlh@bak.com>
2022-07-10 09:30:00 +0800

    first commit

---
+++ src/main/java/com/bak/git/GIt.java
@@ -0,0 +1,11 @@
+package com.bak.git;
+
+/**
+ * @author
+ * @date 2022/7/10  9:25
+ */
+public class GIt {
+    public static void main(String[] args) {
+        System.out.println("Hello,world!");
+    }
+}
