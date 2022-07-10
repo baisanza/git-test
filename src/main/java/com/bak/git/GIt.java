2fa77134971e9e266b5fd68bc7a97dfaf9cf0afa
jlh <jlh@bak.com>
2022-07-10 09:40:37 +0800

    hot-fix commit1

--- src/main/java/com/bak/git/GIt.java
+++ src/main/java/com/bak/git/GIt.java
@@ -8,5 +8,6 @@ public class GIt {
     public static void main(String[] args) {
         System.out.println("Hello,world!");
         System.out.println("Hello,git!");
+        System.out.println("Hello,git2!");
     }
 }
