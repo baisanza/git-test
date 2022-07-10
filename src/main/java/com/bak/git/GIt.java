7f16553b238bf7aa9fc108c2f48df02cdaa147b9
jlh <jlh@bak.com>
2022-07-10 09:32:01 +0800

    second commit

--- src/main/java/com/bak/git/GIt.java
+++ src/main/java/com/bak/git/GIt.java
@@ -7,5 +7,6 @@ package com.bak.git;
 public class GIt {
     public static void main(String[] args) {
         System.out.println("Hello,world!");
+        System.out.println("Hello,git!");
     }
 }
